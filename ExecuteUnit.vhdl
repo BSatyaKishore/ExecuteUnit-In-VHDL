c2cd4a31dad4c90011f63c19f0ba10dce6c135db
Bhargav Reddy <bhargav431994@gmail.com>
2016-10-14 17:45:23 +0530

    2. Working Code

--- ExecuteUnit.vhdl
+++ ExecuteUnit.vhdl
@@ -1,4 +1,6 @@
 library ieee;
+use IEEE.NUMERIC_STD.ALL;
+--use ieee.std_logic_arith.all;
 use ieee.std_logic_1164.all;
 
 entity ExecuteUnit is
@@ -8,50 +10,50 @@ entity ExecuteUnit is
 		ALUResult: out std_logic_vector(31 downto 0);
 		isLSInstruction: out std_logic;
 		isLoad: out std_logic;
-		CompareBits: out std_logic_vector(2 downto 0);
+		CompareBits: out std_logic_vector(1 downto 0);
 		validCompareBits: out std_logic;
-		outROBEntryNo: in std_logic_vector(7 downto 0)
+		outROBEntryNo: out std_logic_vector(7 downto 0);
+		clk: in std_logic
 	);
 end entity ExecuteUnit;
 
 architecture ALU of ExecuteUnit is
 	signal opcode: std_logic_vector(4 downto 0);
 	signal temp	 :std_logic_vector(63 downto 0);
+	signal op1, op2: std_logic_vector(31 downto 0);
 	begin
-		process(ROBInstruction, ROBEntryNo)
-		variable tempInt: integer := 0;
-		begin
-			opcode <= ROBInstruction(4 downto 0);
-			op1 <= ROBInstruction(37 downto 6);
-			op2 <= ROBInstruction(70 downto 39);
-			outROBEntryNo <= ROBEntryNo;
-
-			--! Initilizing signals
-			ALUResult <= x"00000000";
-			validCompareBits <= '0';
-			isLSInstruction <= '0';
-			isLoad <= '0';
+		opcode <= ROBInstruction(4 downto 0);
+		op1 <= ROBInstruction(37 downto 6);
+		op2 <= ROBInstruction(70 downto 39);
+		outROBEntryNo <= ROBEntryNo;
+		process(opcode, op1, op2, clk)
+			variable tempInt: integer := 0;
+			begin
+				if (clk='1') then
+					--! Initilizing signals
+					ALUResult <= x"00000000";
+					validCompareBits <= '0';
+					isLSInstruction <= '0';
+					isLoad <= '0';
 
 			case opcode is
 				when "00000" =>
-					ALUResult <= op1 + op2;
+					ALUResult <= std_logic_vector(to_signed((to_integer(signed(op1)) + to_integer(signed(op2))),32));
 				when "00001" =>
-					ALUResult <= op1 + (not op2) + 1;
-				when "00010" =>	 
-					temp <= signed(op1) * signed(op2);
-					ALUResult <= temp(63) & temp(30 downto 0);
+					ALUResult <= std_logic_vector(to_signed((to_integer(signed(op1)) + to_integer(signed(not op2))+ 1),32));
+				when "00010" =>
+					ALUResult <= std_logic_vector(to_signed((to_integer(signed(op1)) * to_integer(signed(op2))),32));
 				when "00011" =>
-					ALUResult <= divide ( unsigned(op1) , unsigned(op2) );
+					ALUResult <= std_logic_vector(to_signed((to_integer(signed(op1)) / to_integer(signed(op2))),32));
 				when "00100" =>
-					ALUResult <= to_integer(signed(op1)) mod to_integer(signed(op2));
+					ALUResult <= std_logic_vector(to_signed((to_integer(signed(op1)) mod to_integer(signed(op2))),32));
 				when "00101" =>
 					validCompareBits <= '1';
+					CompareBits <= b"00";
 					if (op1=op2) then
 						CompareBits <= b"01";
 					elsif (to_integer(signed(op1)) > to_integer(signed(op2))) then
 						CompareBits <= b"10";
-					else
-						CompareBits <= b"00";
 					end if;
 				when "00110" =>
 					ALUResult <= (op1 and op2);
@@ -62,19 +64,22 @@ architecture ALU of ExecuteUnit is
 				when "01001" =>
 					ALUResult <= (op1);
 				when "01010" =>
-					ALUResult <= (op1 sll to_int(op2));
+					ALUResult <= (op1); -- >> to_integer(signed(op2)));
 				when "01011" =>
-					ALUResult <= (op1 srl to_int(op2));
-				when "01100" =>
-					--! ASR
+					ALUResult <= (op1); -- << to_integer(signed(op2)));
+--				when "01100" =>
+--					--! ASR
 				when "01101" =>
 					isLSInstruction <= '1';
 					isLoad <= '1';
-					ALUResult <= op1 + op2;
+					ALUResult <= std_logic_vector(to_signed((to_integer(signed(op1)) + to_integer(signed(op2))),32));
 				when "01110" =>
 					isLSInstruction <= '1';
-					ALUResult <= op1 + op2;
-				--! TODO: branch instructions over here ALUResult <= 
+					ALUResult <= std_logic_vector(to_signed((to_integer(signed(op1)) + to_integer(signed(op2))),32));
+--				--! TODO: branch instructions over here ALUResult <=
+				when others =>
+					ALUResult <= std_logic_vector(to_signed((to_integer(signed(op1)) + to_integer(signed(op2))),32));
 			end case;
+		end if;
 		end process;
 end architecture ALU;
--- Main.vhdl
+++ Main.vhdl
@@ -13,21 +13,23 @@ architecture MainArch of Main is
                 ALUResult: out std_logic_vector(31 downto 0);
                 isLSInstruction: out std_logic;
                 isLoad: out std_logic;
-                CompareBits: out std_logic_vector(2 downto 0);
+                CompareBits: out std_logic_vector(1 downto 0);
                 validCompareBits: out std_logic;
-                outROBEntryNo: out std_logic_vector;
+                outROBEntryNo: out std_logic_vector(7 downto 0);
+		clk: in std_logic
         );
         end component ExecuteUnit;
 
-        signal ROBInstruction: std_logic_vector(110 downto 0) := b"00000000000000000000000000000000000000000000000000000000000000000000101100000000000000000000000000000101100000";
+        signal ROBInstruction: std_logic_vector(110 downto 0) := b"000000000000000000000000000000000000000000000000000000000000000000000101100000000000000000000000000000101100101";
         signal ROBEntryNo: std_logic_vector(7 downto 0) := b"00000010";
         signal ALUResult: std_logic_vector(31 downto 0);
-        isLSInstruction: std_logic;
-        isLoad: std_logic;
-        CompareBits: std_logic_vector(2 downto 0);
-        validCompareBits: std_logic;
-        outROBEntryNo: std_logic_vector;
+        signal isLSInstruction: std_logic;
+        signal isLoad: std_logic;
+        signal CompareBits: std_logic_vector(1 downto 0);
+        signal validCompareBits: std_logic;
+        signal outROBEntryNo: std_logic_vector(7 downto 0);
+	signal clk: std_logic := '0';
 begin
-	Main : ExecuteUnit port map(ROBInstruction, ROBEntryNo, ALUResult, isLSInstruction, isLoad, CompareBits, validCompareBits, outROBEntryNo);
+	Main : ExecuteUnit port map(ROBInstruction, ROBEntryNo, ALUResult, isLSInstruction, isLoad, CompareBits, validCompareBits, outROBEntryNo,clk);
+	clk <= '0' after 9 ns when clk='1' else not clk after 1 ns;
 end MainArch;
-
